c8c0cce59d3a79ff1145866ecb5e34123e6310c4
Devendra Kayande <95514274+MysticShadow427@users.noreply.github.com>
2022-10-06 15:29:48 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var app = http.createServer(function(req, res) {
     res.end(index);
 });
 
-// used variables to handle each event and listened using socket.io and then write to serial port of Arduino
+// used variables to handle each event emmited when we enter value in our forms and listened using socket.io and then write to serial port of Arduino
 var io1 = require('socket.io').listen(app);
 
 io1.on('connection', function(socket) {
@@ -36,69 +36,41 @@ io1.on('connection', function(socket) {
     
     });
     
-});
-
-var io2=require('socket.io').listen(app);
-
-io2.on('connection',function(socket){
     socket.on('shadetime',function(data){
         console.log(data);
         port.write(data.time_interval);
     });
-});
-
-var io3=require('socket.io').listen(app);
-
-io3.on('connection',function(socket){
+    
     socket.on('shade1',function(data){
         console.log(data);
         port.write(data.rgb_value1);
     });
-});
-
-var io4=require('socket.io').listen(app);
-
-io4.on('connection',function(socket){
+    
     socket.on('timer',function(data){
         console.log(data);
         port.write(data.time_red);
     });
-});
-
-var io5=require('socket.io').listen(app);
-
-io5.on('connection',function(socket){
+    
     socket.on('shade2',function(data){
         console.log(data);
         port.write(data.rgb_value2);
-    })
-});
-
-var io6=require('socket.io').listen(app);
-
-io6.on('connection',function(socket){
+    });
+    
     socket.on('timeg',function(data){
         console.log(data);
         port.write(data.time_green);
     });
-});
-
-
-var io7=require('socket.io').listen(app);
-
-io7.on('connection',function(socket){
+    
     socket.on('shade3',function(data){
         console.log(data);
         port.write(data.rgb_value3);
     });
-});
-
-var io8=require('socket.io').listen(app);
-
-io8.on('connection',function(socket){
+    
     socket.on('timeb',function(data){
         console.log(data);
         port.write(data.time_blue);
     });
+    
 });
+
 app.listen(port_number);
